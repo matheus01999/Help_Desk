507bf8033dc4df0ecaed7b67273da24be9b879de
Matheus_Rocha <matheus.rocha01999@outlook.com>
2024-11-12 21:27:27 -0300

    Refatorando tudo

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dev/help',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'bfbb116a67a225b31a4cf609ad3900f3ea46d910',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dev/help' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'bfbb116a67a225b31a4cf609ad3900f3ea46d910',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
