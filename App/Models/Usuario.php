bfc69a852a2e4d069f9df9e31f3337bf1546cb05
Matheus Rocha <matheus.rocha01999@outlook.com>
2025-07-20 16:46:43 -0300

    Doc: comentado o Modelo Usuario

--- App/Models/Usuario.php
+++ App/Models/Usuario.php
@@ -13,16 +13,17 @@ class Usuario extends Model {
 	private $email;
 	private $senha;
 
+	// METODO MAGICO QUE RECUPERA OS VALORES DO OBJETO
 	public function __get($atributo) {
 		return $this->$atributo;
 	}
 
+	// METODO MAGICO QUE SETAR OS VALORES DO OBJETO
 	public function __set($atributo, $valor) {
 		$this->$atributo = $valor;
 	}
-
 	
-	//recuperar um usuário por e-mail
+	// MEOTODO RESPONSAVEL POR RECUPERAR UM USUARIO POR E-MAIL
 	public function getUsuarioPorEmail() {
 		$query = "select nome, email from usuarios where email = :email";
 		$stmt = $this->db->prepare($query);
@@ -32,6 +33,7 @@ class Usuario extends Model {
 		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
 	}
 
+	// METODO RESPONSAVEL POR AUTENTICAR UM USUARIO
 	public function autenticar(){
 		$query = "select id, nome, email from usuarios where email = :email and senha = :senha";
 		$stmt = $this->db->prepare($query);
@@ -50,6 +52,7 @@ class Usuario extends Model {
 		return $this;
 	}
 
+	// METODO RESPONSAVEL POR LISTAR OS USUARIOS
 	public function listar(){
 		$query = "select id, nome, email from usuarios";
 		$stmt = $this->db->prepare($query);
@@ -59,6 +62,7 @@ class Usuario extends Model {
 
 	}
 
+	// METODO RESPONSAVEL POR EXCLUIR UM USUSARIOS DO BANCO
 	public function excluir(){
 		$query = 'delete from usuarios where id = :id';
 		$stmt = $this->db->prepare($query);
